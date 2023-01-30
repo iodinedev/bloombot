3b773891900fd80aeff15e7665069737b9fc91c5
Zachary Montgomery <zachmontgo@iodinedev.com>
2023-01-30 14:15:36 -0600

    It's about drie

--- src/commands/quotes.ts
+++ src/commands/quotes.ts
@@ -11,15 +11,14 @@ export = {
     const quote = await database.quoteBook.findMany();
     const random = Math.floor(Math.random() * quote.length);
 
-    if (quote.length === 0) return interaction.reply({ content: ':x: No quotes found. Here\'s a backup: "*And those who were seen dancing were thought to be insane by those who could not hear the music.*"', ephemeral: true });
+    if (quote.length === 0) return interaction.reply({ content: ':x: No quotes found.', ephemeral: true });
 
     const quoteText = quote[random].quote;
     const quoteAuthor = quote[random].author;
 
     const quoteEmbed = new EmbedBuilder()
       .setColor(config.embedColor)
-      .setDescription(`*${quoteText}*`)
-      .setFooter({ text: `- ${quoteAuthor}` });
+      .setDescription(`*${quoteText}*\n- ${quoteAuthor}`);
 
 		await interaction.reply({ embeds: [quoteEmbed] });
 	},
