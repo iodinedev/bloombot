6c673484a2c4fc0c178b0fdf88d7acd69717ef0c
Zachary Montgomery <zachmontgo@iodinedev.com>
2023-07-27 10:08:17 -0500

    Start rust rework

--- src/discord.d.ts
+++
@@ -1,8 +0,0 @@
-import { type Collection } from 'discord.js'
-
-declare module 'discord.js' {
-  export interface Client {
-    commands: Collection<unknown, any>
-    modules: Record<string, string[]>
-  }
-}
