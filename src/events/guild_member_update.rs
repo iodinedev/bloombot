bd5034882c22c7fe202152d03de10b73a07b24b9
Meditation Mind <115960723+meditationmind@users.noreply.github.com>
2023-11-21 00:35:57 +0900

    Fix guild member events
    Set proper intents, remove DM on join, update embeds, implement second donator role

--- src/events/guild_member_update.rs
+++ src/events/guild_member_update.rs
@@ -3,16 +3,17 @@ use anyhow::Result;
 use poise::serenity_prelude::{self as serenity, Context, Member};
 
 enum UpdateType {
-  BecamePatron,
+  BecameDonator,
   StoppedPending,
 }
 
 impl UpdateType {
   fn get_type(old: &Member, new: &Member) -> Option<Self> {
-    let patron_role = serenity::RoleId(config::ROLES.patreon);
+    let patreon_role = serenity::RoleId(config::ROLES.patreon);
+    let kofi_role = serenity::RoleId(config::ROLES.kofi);
 
-    if !old.roles.contains(&patron_role) && new.roles.contains(&patron_role) {
-      Some(Self::BecamePatron)
+    if (!old.roles.contains(&patreon_role) && !old.roles.contains(&kofi_role)) && (new.roles.contains(&patreon_role) || new.roles.contains(&kofi_role)) {
+      Some(Self::BecameDonator)
     } else if old.pending && !new.pending {
       Some(Self::StoppedPending)
     } else {
@@ -33,16 +34,16 @@ pub async fn guild_member_update(
 
   if let Some(update_type) = UpdateType::get_type(old, new) {
     match update_type {
-      UpdateType::BecamePatron => {
-        let patron_channel = serenity::ChannelId(config::CHANNELS.patreon);
+      UpdateType::BecameDonator => {
+        let donator_channel = serenity::ChannelId(config::CHANNELS.donators);
 
-        patron_channel
+        donator_channel
           .send_message(&ctx, |m| {
             m.embed(|e| {
               crate::config::BloomBotEmbed::from(e)
-                .title("New Patron")
+                .title(":tada: New Donator :tada:")
                 .description(format!(
-                  "Please welcome {} as a new Patron.\n\nThank you for your generosity, it help keeps this server running!",
+                  "Please welcome {} as a new donator.\n\nThank you for your generosity! It help keeps this community alive <:loveit:579017125809881089>",
                   new.user.tag()
                 ))
             })
@@ -54,13 +55,15 @@ pub async fn guild_member_update(
 
         welcome_channel
           .send_message(&ctx, |m| {
-            m.embed(|e| {
-              config::BloomBotEmbed::from(e)
-                .title("New Member")
-                .description(format!(
-                  ":tada: **A new member has arrived!** :tada:\nPlease welcome {} to the Meditation Mind Discord <@&828291690917265418>!\nWe're glad you've joined us. <:aww:578864572979478558>",
-                  new.user.tag()
-                ))
+            m.content(format!("Please give {} a warm welcome, <@&{}>!", new.user.tag(), config::ROLES.welcome_team))
+              .embed(|e| {
+                config::BloomBotEmbed::from(e)
+                  .title(":tada: A new member has arrived! :tada:")
+                  .description(format!(
+                    "Welcome to the Meditation Mind community, {}!\n\nCheck out <id:customize> to grab some roles and customize your community experience.\n\nWe're glad you've joined us! <:aww:578864572979478558>",
+                    new.user.tag()
+                  ))
+                  .thumbnail("https://meditationmind.org/wp-content/uploads/2020/04/Webp.net-resizeimage-1.png")
             })
           })
           .await?;
--- src/events/mod.rs
+++ src/events/mod.rs
@@ -1,11 +1,11 @@
-mod guild_member_addition;
+// mod guild_member_addition;
 mod guild_member_removal;
 mod guild_member_update;
 mod message_delete;
 mod reaction_add;
 mod reaction_remove;
 
-pub use guild_member_addition::guild_member_addition;
+// pub use guild_member_addition::guild_member_addition;
 pub use guild_member_removal::guild_member_removal;
 pub use guild_member_update::guild_member_update;
 pub use message_delete::message_delete;
